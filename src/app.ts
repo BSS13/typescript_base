532380f9296acbeb0ed121a28392294be33cb49a
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2022-05-15 21:27:59 +1000

    Added member functions in the department class

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -9,12 +10,25 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const department = new Department("IT");
+
+department.addEmployee("ABC");
+department.addEmployee("DEF");
 department.describe();
+department.printEmployeeInformation();
 
 //To handle these scenarios, we add the type of this keyword in the describe function in the class
 //If we add the name property, it works well - compiles down to old JS function type compilation
-const departmentCopy = { name: "DUMMY", describe: department.describe };
-departmentCopy.describe();
+// const departmentCopy = { name: "DUMMY", describe: department.describe };
+// departmentCopy.describe();
