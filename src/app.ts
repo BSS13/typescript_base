b5347f67c29c78d943a60cd0863582fa2db99011
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2022-05-21 15:33:58 +1000

    Added lenghy generic function example for typechecking

--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,19 @@ console.log(mergedObj.name);
 
 const mergedObj1 = merge({ name: "Bob" }, { age: 23 });
 console.log(mergedObj1);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got " + element.length + " element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
