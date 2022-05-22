b79c8f06839b8936ac048e9830bbf46aaec83379
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2022-05-22 13:35:48 +1000

    Setup validation check for course class using decorators

--- src/app.ts
+++ src/app.ts
@@ -112,3 +112,42 @@ class Printer {
 const p = new Printer();
 const buttonElement = document.querySelector("button")!;
 buttonElement.addEventListener("click", p.showMessage);
+
+//------
+//Decorators with Validations
+
+function Required() {}
+function PositiveNumber() {}
+
+function validate(obj: object): boolean {
+  return false;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector("form");
+courseForm?.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+  if (!validate(createdCourse)) {
+    alert("Invalid input, please try again!!");
+    return;
+  }
+  console.log(createdCourse);
+});
