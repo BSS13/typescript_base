93595eb392014e8969ef7dab35584d5b3bca45a7
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2022-06-06 20:51:29 +1000

    Merged function overloading and functions

--- src/functions.ts
+++ src/functions.ts
@@ -26,3 +26,20 @@ console.log(combineValues(8, 7));
 addAndHandle(10, 20, (result) => {
   console.log(result);
 });
+
+type CombinableType = number | string;
+
+function addValues(a: number, b: number): number;
+function addValues(a: string, b: string): string;
+function addValues(a: string, b: number): string;
+function addValues(a: number, b: string): string;
+function addValues(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = addValues("Bal", "SS");
+console.log(result?.toUpperCase);
+
